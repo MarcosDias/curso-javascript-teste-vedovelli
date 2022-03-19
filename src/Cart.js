824b158361495171b6088221bc4343d61d06e6a8
Marcos Dias <marcos.a.melo.dias@gmail.com>
2022-03-19 16:40:09 -0300

    Teste e implementação do metodo checkout e introdução aos snapshots

--- src/Cart.js
+++ src/Cart.js
@@ -19,4 +19,11 @@ export default class Cart {
       return acc + item.quantity * item.product.price;
     }, 0);
   }
+
+  checkout() {
+    return {
+      total: this.getTotal(),
+      items: this.items,
+    };
+  }
 }
