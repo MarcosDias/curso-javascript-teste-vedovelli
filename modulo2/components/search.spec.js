bb2aa36ca60f193c6b77e01c6eca71ab54646965
Marcos Dias <marcos.a.melo.dias@gmail.com>
2022-03-24 09:52:46 -0300

    Finalizaçao testes componente Search

--- modulo2/components/search.spec.js
+++ modulo2/components/search.spec.js
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Search from './search';
 
 const doSearch = jest.fn();
@@ -10,6 +11,12 @@ describe('Search', () => {
     expect(screen.getByRole('form')).toBeInTheDocument();
   });
 
+  it('should render input type equals search', () => {
+    render(<Search doSearch={doSearch} />);
+
+    expect(screen.getByRole('searchbox')).toHaveProperty('type', 'search');
+  });
+
   it('should call props.doSearch() when form is submitted', () => {
     render(<Search doSearch={doSearch} />);
 
@@ -18,5 +25,18 @@ describe('Search', () => {
     fireEvent.submit(form);
 
     expect(doSearch).toHaveBeenCalled();
+  });
+
+  it('should call props.doSearch() with the user input', () => {
+    render(<Search doSearch={doSearch} />);
+
+    const inputText = 'some text here';
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    userEvent.type(input, inputText);
+    fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledWith(inputText);
   });
 });
