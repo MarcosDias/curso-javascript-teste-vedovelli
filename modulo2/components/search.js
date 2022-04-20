24de03f7f3b401ae2d76de1d6e60d0b0bf6579df
Marcos Dias <marcos.a.melo.dias@gmail.com>
2022-04-20 07:56:50 -0300

    Mais testes no componente search no fluxo de limpar o input de pesquisa

--- modulo2/components/search.js
+++ modulo2/components/search.js
@@ -2,12 +2,22 @@ import { useState } from 'react';
 
 export default function Search({ doSearch }) {
   const [term, setTerm] = useState('');
+
+  const submitHandler = (ev) => {
+    ev.preventDefault();
+    doSearch(term);
+  };
+
+  const inputHandler = (ev) => {
+    setTerm(ev.target.value);
+
+    if (ev.target.value === '') {
+      doSearch('');
+    }
+  };
+
   return (
-    <form
-      onSubmit={() => doSearch(term)}
-      name="form-search"
-      className="relative mt-6 max-w-lg mx-auto"
-    >
+    <form onSubmit={submitHandler} name="form-search" className="relative mt-6 max-w-lg mx-auto">
       <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
         <svg className="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
           <path
@@ -19,10 +29,10 @@ export default function Search({ doSearch }) {
           />
         </svg>
       </span>
-
+      With('');
       <input
         value={term}
-        onChange={(e) => setTerm(e.target.value)}
+        onInput={inputHandler}
         className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
         type="search"
         placeholder="Search"
--- modulo2/components/search.unit.spec.js
+++ modulo2/components/search.unit.spec.js
@@ -5,6 +5,10 @@ import Search from './search';
 const doSearch = jest.fn();
 
 describe('Search', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should render a form', () => {
     render(<Search doSearch={doSearch} />);
 
@@ -38,5 +42,18 @@ describe('Search', () => {
     fireEvent.submit(form);
 
     expect(doSearch).toHaveBeenCalledWith(inputText);
+  });
+
+  it('should call props.doSearch() when search is cleared', () => {
+    render(<Search doSearch={doSearch} />);
+
+    const inputText = 'some text here';
+    const input = screen.getByRole('searchbox');
+
+    userEvent.type(input, inputText);
+    userEvent.clear(input);
+
+    expect(doSearch).toHaveBeenCalledTimes(1);
+    expect(doSearch).toHaveBeenCalledWith('');
   });
 });
