a7b4c0bbf09c8ce5d94ff9ff9f0a48f4cf374841
Marcos Dias <marcos.a.melo.dias@gmail.com>
2022-04-22 02:20:53 -0300

    Novas action no cart store - remove, removeAll, increase e decrease

--- modulo2/store/cart/cart-store.unit.spec.js
+++ modulo2/store/cart/cart-store.unit.spec.js
@@ -7,12 +7,20 @@ describe('Cart Store', () => {
   let cartStore;
   let add;
   let toggle;
+  let remove;
+  let removeAll;
+  let increase;
+  let decrease;
 
   beforeEach(() => {
     server = makeServer({ environment: 'test' });
     cartStore = renderHook(() => useCartStore()).result;
     add = cartStore.current.actions.add;
     toggle = cartStore.current.actions.toggle;
+    remove = cartStore.current.actions.remove;
+    removeAll = cartStore.current.actions.removeAll;
+    increase = cartStore.current.actions.increase;
+    decrease = cartStore.current.actions.decrease;
   });
 
   afterEach(() => {
@@ -41,7 +49,7 @@ describe('Cart Store', () => {
     expect(cartStore.current.state.products).toHaveLength(0);
   });
 
-  it('should add 2 products to the list', async () => {
+  it('should add 2 products to the list and open the cart', async () => {
     const products = server.createList('product', 2);
 
     for (const product of products) {
@@ -49,6 +57,7 @@ describe('Cart Store', () => {
     }
 
     expect(cartStore.current.state.products).toHaveLength(2);
+    expect(cartStore.current.state.open).toBe(true);
   });
 
   it('should not add same product twice', () => {
@@ -58,5 +67,102 @@ describe('Cart Store', () => {
     act(() => add(product));
 
     expect(cartStore.current.state.products).toHaveLength(1);
+  });
+
+  it('should assign 1 as initial quantity on product add()', () => {
+    const product = server.create('product');
+
+    act(() => {
+      add(product);
+    });
+
+    expect(cartStore.current.state.products[0].quantity).toBe(1);
+  });
+
+  it('should increase quantity', () => {
+    const product = server.create('product');
+
+    act(() => {
+      add(product);
+      increase(product);
+    });
+
+    expect(cartStore.current.state.products[0].quantity).toBe(2);
+  });
+
+  it('should increase quantity', () => {
+    const product = server.create('product');
+
+    act(() => {
+      add(product);
+      decrease(product);
+    });
+
+    expect(cartStore.current.state.products[0].quantity).toBe(0);
+  });
+
+  it('should NOT decrease below zero', () => {
+    const product = server.create('product');
+
+    act(() => {
+      add(product);
+      decrease(product);
+      decrease(product);
+    });
+
+    expect(cartStore.current.state.products[0].quantity).toBe(0);
+  });
+
+  it('should remove a product from the store', () => {
+    const [product1, product2] = server.createList('product', 2);
+
+    act(() => {
+      add(product1);
+      add(product2);
+    });
+
+    expect(cartStore.current.state.products).toHaveLength(2);
+
+    act(() => {
+      remove(product1);
+    });
+
+    expect(cartStore.current.state.products).toHaveLength(1);
+    expect(cartStore.current.state.products[0]).toEqual(product2);
+  });
+
+  it('should not change products in the cart if provided product is not in the array', () => {
+    const [product1, product2, product3] = server.createList('product', 3);
+
+    act(() => {
+      add(product1);
+      add(product2);
+    });
+
+    expect(cartStore.current.state.products).toHaveLength(2);
+
+    act(() => {
+      remove(product3);
+    });
+
+    expect(cartStore.current.state.products).toHaveLength(2);
+  });
+
+  it('should clear cart', () => {
+    const products = server.createList('product', 2);
+
+    act(() => {
+      for (const product of products) {
+        add(product);
+      }
+    });
+
+    expect(cartStore.current.state.products).toHaveLength(2);
+
+    act(() => {
+      removeAll();
+    });
+
+    expect(cartStore.current.state.products).toHaveLength(0);
   });
 });
--- modulo2/store/cart/index.js
+++ modulo2/store/cart/index.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import create from 'zustand';
 
 const initialState = {
@@ -5,19 +6,69 @@ const initialState = {
   products: [],
 };
 
-const addProduct = (store, product) => {
-  if (store.state.products.includes(product)) {
-    return store.state.products;
-  }
-  return [...store.state.products, product];
-};
+export const useCartStore = create((set) => {
+  const setState = (fn) => set(produce(fn));
 
-export const useCartStore = create((set) => ({
-  state: { ...initialState },
-  actions: {
-    reset: () => set((store) => ({ state: { ...initialState } })),
-    toggle: () => set((store) => ({ state: { ...store.state, open: !store.state.open } })),
-    add: (product) =>
-      set((store) => ({ state: { open: true, products: addProduct(store, product) } })),
-  },
-}));
+  return {
+    state: {
+      ...initialState,
+    },
+    actions: {
+      reset() {
+        setState((store) => {
+          store.state = initialState;
+        });
+      },
+      toggle() {
+        setState(({ state }) => {
+          state.open = !state.open;
+        });
+      },
+      add(product) {
+        setState(({ state }) => {
+          const doesntExist = !state.products.find(({ id }) => id === product.id);
+
+          if (doesntExist) {
+            if (!product.quantity) {
+              product.quantity = 1;
+            }
+            state.products.push(product);
+            state.open = true;
+          }
+        });
+      },
+      increase(product) {
+        setState(({ state }) => {
+          const localProduct = state.products.find(({ id }) => id === product.id);
+          if (localProduct) {
+            localProduct.quantity++;
+          }
+        });
+      },
+      decrease(product) {
+        setState(({ state }) => {
+          const localProduct = state.products.find(({ id }) => id === product.id);
+          if (localProduct && localProduct.quantity > 0) {
+            localProduct.quantity--;
+          }
+        });
+      },
+      remove(product) {
+        setState(({ state }) => {
+          const exists = !!state.products.find(({ id }) => id === product.id);
+
+          if (exists) {
+            state.products = state.products.filter(({ id }) => {
+              return id !== product.id;
+            });
+          }
+        });
+      },
+      removeAll() {
+        setState(({ state }) => {
+          state.products = [];
+        });
+      },
+    },
+  };
+});
